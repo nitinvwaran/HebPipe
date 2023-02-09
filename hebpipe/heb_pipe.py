21d7be1f63a0fbd20641c0cc1dd57b39b9b71832
nitin <nitin.vwaran@gmail.com>
2023-02-10 02:01:40 +0800

    final

--- hebpipe/heb_pipe.py
+++ hebpipe/heb_pipe.py
@@ -610,7 +610,7 @@ def nlp(input_data, do_whitespace=True, do_tok=True, do_tag=True, do_lemma=True,
     bound_group_map = get_bound_group_map(tokenized) if out_mode == "conllu" else None
 
     if mtltagger:
-        tagged_conllu, tokenized, morphs, words,lemmas = mtltagger.predict(tokenized,sent_tag=sent_tag,checkpointfile='/home/nitin/Desktop/hebpipe/HebPipe/hebpipe/data/checkpoint/soft_l2_htb_best_mtlmodel_4.555484_0.930185_0.980181_0.659548_0.967692.pt')
+        tagged_conllu, tokenized, morphs, words,lemmas = mtltagger.predict(tokenized,sent_tag=sent_tag,checkpointfile='/home/nitin/Desktop/hebpipe/HebPipe/hebpipe/data/checkpoint/mtl_ml_soft_htb_best_mtlmodel_5.373966_0.92623_0.98_0.655294_0.960271.pt')
 
     if out_mode == "pipes":
         return tokenized
